cea955da0c74024b57f8d7cb5d20789885b48871
brianchangyuhao <brian.chang@logicsolutions.com>
2018-07-15 22:29:10 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,87 @@
+import cx from 'classnames';
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+let index = 0;
+
+const TodoForm = ({addTodo}) => {
+	let input;
+  
+	return (
+	  <div>
+		<input ref={node => {
+		  input = node;
+		}} />
+		<button onClick={() => {
+		  addTodo(input.value);
+		  input.value = '';
+		}}>
+		  Add
+		</button>
+	  </div>
+	);
+  };
+  
+const Todo = ({todo, clickTodo}) => {
+	return (<li onClick={() => clickTodo(todo.id)} className={cx({ 'is-done': todo.done})}>{todo.text}</li>);
+}
+  
+const TodoLists = ({todos, clickTodo}) => {
+	const todoNode = todos.map((todo) => {
+		return (<Todo todo={todo} key={todo.id} clickTodo={clickTodo}/>)
+	});
+	return (<ul>{todoNode}</ul>);
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+  	constructor(props){
+		super(props);
+		this.state = {
+			data: []
+		}
+	}
+	  
+	addTodo = val => {
+		// to prevent from empty string
+        if (!val.trim()) {
+          return
+        }
+        const todo = {text: val, id: index++, done: false}
+        this.setState({data: [...this.state.data, todo]});
+    }
+    
+    handleClickTodo = id => {
+        const remainder = this.state.data.map(todo => {
+			if(todo.id === id) {
+				return {...todo, done: !todo.done};
+			} else {
+				return todo;
+			}
+        });
+        this.setState({data: remainder});
+    }
+
+	render() {
+		return (
+			<div>
+				<div>
+					<h2>
+						Todo List
+					</h2>
+					<TodoForm addTodo={this.addTodo}/>
+                    <TodoLists 
+                      todos={this.state.data} 
+                      clickTodo={this.handleClickTodo}
+                    />
+					{`${this.state.data.filter(todo => todo.done === false).length} remaining out of ${this.state.data.length} tasks`}
+				</div>
+				<style>{`
+					.is-done {
+					text-decoration: line-through;
+					}
+				`}</style>
+			</div>
+		);
+	}
 }
 
 export default App;
